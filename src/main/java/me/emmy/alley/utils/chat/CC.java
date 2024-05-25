24a316b49491ba33414f6ab9a024f7b3a7245e1f
Emmy <lisariesey@gmail.com>
2024-05-25 12:48:08 +0200

    feat: Delete CC and wrote my fucking own because using praxi's is apparently "skidding" ?

--- src/main/java/me/emmy/alley/utils/chat/CC.java
+++ src/main/java/me/emmy/alley/utils/chat/CC.java
@@ -5,100 +5,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Emmy
- * Project: FlowerCore
- * Discord: dsc.gg/emmiesa
+ * Project: Alley
+ * Date: 25/05/2024 - 12:41
  */
 
 public class CC {
-
-    public static final String BLUE;
-    public static final String AQUA;
-    public static final String YELLOW;
-    public static final String RED;
-    public static final String GRAY;
-    public static final String GOLD;
-    public static final String GREEN;
-    public static final String WHITE;
-    public static final String BLACK;
-    public static final String BOLD;
-    public static final String ITALIC;
-    public static final String UNDER_LINE;
-    public static final String STRIKE_THROUGH;
-    public static final String RESET;
-    public static final String MAGIC;
-    public static final String DARK_BLUE;
-    public static final String DARK_AQUA;
-    public static final String DARK_GRAY;
-    public static final String DARK_GREEN;
-    public static final String DARK_PURPLE;
-    public static final String DARK_RED;
-    public static final String PINK;
-    public static final String MENU_BAR;
-    public static final String CHAT_BAR;
-    public static final String SB_BAR;
-    public static final String TAB_BAR;
-    public static final String FLOWER_BAR;
-    public static final String FLOWER_BAR_LONG;
-    public static final String FLOWER_BAR_VERY_LONG;
-    public static final String FLOWER_BAR_LONG_RED;
-    private static final Map<String, ChatColor> MAP;
-
-    static {
-        MAP = new HashMap<>();
-        MAP.put("pink", ChatColor.LIGHT_PURPLE);
-        MAP.put("orange", ChatColor.GOLD);
-        MAP.put("purple", ChatColor.DARK_PURPLE);
-
-        for (ChatColor chatColor : ChatColor.values()) {
-            MAP.put(chatColor.name().toLowerCase().replace("_", ""), chatColor);
-        }
-
-        BLUE = ChatColor.BLUE.toString();
-        AQUA = ChatColor.AQUA.toString();
-        YELLOW = ChatColor.YELLOW.toString();
-        RED = ChatColor.RED.toString();
-        GRAY = ChatColor.GRAY.toString();
-        GOLD = ChatColor.GOLD.toString();
-        GREEN = ChatColor.GREEN.toString();
-        WHITE = ChatColor.WHITE.toString();
-        BLACK = ChatColor.BLACK.toString();
-        BOLD = ChatColor.BOLD.toString();
-        ITALIC = ChatColor.ITALIC.toString();
-        UNDER_LINE = ChatColor.UNDERLINE.toString();
-        STRIKE_THROUGH = ChatColor.STRIKETHROUGH.toString();
-        RESET = ChatColor.RESET.toString();
-        MAGIC = ChatColor.MAGIC.toString();
-        DARK_BLUE = ChatColor.DARK_BLUE.toString();
-        DARK_AQUA = ChatColor.DARK_AQUA.toString();
-        DARK_GRAY = ChatColor.DARK_GRAY.toString();
-        DARK_GREEN = ChatColor.DARK_GREEN.toString();
-        DARK_PURPLE = ChatColor.DARK_PURPLE.toString();
-        DARK_RED = ChatColor.DARK_RED.toString();
-        PINK = ChatColor.LIGHT_PURPLE.toString();
-        MENU_BAR = ChatColor.DARK_GRAY.toString() + ChatColor.STRIKETHROUGH + "------------------------";
-        CHAT_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "------------------------------------------------";
-        SB_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "----------------------";
-        TAB_BAR = ChatColor.GRAY.toString() + ChatColor.STRIKETHROUGH + "-----------------";
-
-        FLOWER_BAR = translate("&d&lೋღ&d&l&m«-------&f&l&m-------&d&l&m-------»&r&d&lღೋ");
-        FLOWER_BAR_LONG = translate("&b&lೋღ&b&l&m«-------&f&l&m-----------------&b&l&m-------»&r&b&lღೋ");
-        FLOWER_BAR_VERY_LONG = translate("&b&lೋღ&b&l&m«-------&f&l&m----------------------------&b&l&m-------»&r&b&lღೋ");
-
-        FLOWER_BAR_LONG_RED = translate("&4&lೋღ&4&l&m«-------&f&l&m-----------------&4&l&m-------»&r&4&lღೋ");
-    }
-
-    public static void sendError(String message) {
-        Bukkit.getServer().getConsoleSender().sendMessage(CC.translate("&f[&cERROR&f] &c" + message + "!"));
-    }
-
-    public static String translate(String in) {
-        return ChatColor.translateAlternateColorCodes('&', in);
+    public static String translate(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
     }
 
     public static List<String> translate(List<String> lines) {
@@ -122,6 +39,20 @@ public class CC {
 
         return toReturn;
     }
+
+    public static void sendError(String message) {
+        Bukkit.getServer().getConsoleSender().sendMessage(CC.translate("&f[&cERROR&f] &c" + message + "!"));
+    }
+
+    public static void broadcast(String text) {
+        Bukkit.broadcastMessage(translate(text));
+    }
+
+    public static String MENU_BAR = translate("&7&m------------------------");
+    public static String FLOWER_BAR = translate("&d&lೋღ&d&l&m«-------&f&l&m-------&d&l&m-------»&r&d&lღೋ");
+    public static String FLOWER_BAR_LONG = translate("&b&lೋღ&b&l&m«-------&f&l&m-----------------&b&l&m-------»&r&b&lღೋ");
+    public static String FLOWER_BAR_VERY_LONG = translate("&b&lೋღ&b&l&m«-------&f&l&m----------------------------&b&l&m-------»&r&b&lღೋ");
+    public static String FLOWER_BAR_LONG_RED = translate("&4&lೋღ&4&l&m«-------&f&l&m-----------------&4&l&m-------»&r&4&lღೋ");
 
     public static void on(long timeTaken) {
         Bukkit.getConsoleSender().sendMessage(" ");
