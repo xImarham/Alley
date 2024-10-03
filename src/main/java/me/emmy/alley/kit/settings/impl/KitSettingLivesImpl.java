fe13da8fd0a056989a6f772f898e95a117d07501
Emmy <lisariesey@gmail.com>
2024-10-03 16:15:04 +0200

    chore: correct typo

--- src/main/java/me/emmy/alley/kit/settings/impl/KitSettingLivesImpl.java
+++ src/main/java/me/emmy/alley/kit/settings/impl/KitSettingLivesImpl.java
@@ -8,6 +8,6 @@ import me.emmy.alley.kit.settings.annotation.KitSettingData;
  * @project Alley
  * @date 21/09/2024 - 19:45
  */
-@KitSettingData(name = "Lives", description = "This kit setting enabled best of 3 type matches, such as Battle Rush.", enabled = false)
+@KitSettingData(name = "Lives", description = "This kit setting enables best of 3 type matches, such as Battle Rush.", enabled = false)
 public class KitSettingLivesImpl extends KitSetting {
 }
