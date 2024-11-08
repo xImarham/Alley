bee92de89d93072b25a3c354c63189db512ffd15
Emmy <lisariesey@gmail.com>
2024-11-08 16:10:21 +0100

    feat(arena-command): clickable page navigation

--- src/main/java/dev/revere/alley/arena/command/ArenaCommand.java
+++ src/main/java/dev/revere/alley/arena/command/ArenaCommand.java
@@ -11,7 +11,9 @@ import dev.revere.alley.api.command.BaseCommand;
 import dev.revere.alley.api.command.Command;
 import dev.revere.alley.api.command.CommandArgs;
 import dev.revere.alley.api.command.Completer;
+import dev.revere.alley.util.chat.ClickableUtil;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,30 +72,61 @@ public class ArenaCommand extends BaseCommand {
         return completion;
     }
 
+    @Command(name = "arena", permission = "alley.admin", inGameOnly = false)
     @Override
-    @Command(name = "arena", permission = "alley.admin")
-    public void onCommand(CommandArgs args) {
-        CommandSender player = args.getPlayer();
+    public void onCommand(CommandArgs command) {
+        CommandSender sender = command.getSender();
+        String[] args = command.getArgs();
+        int page = 1;
 
-        player.sendMessage(" ");
-        player.sendMessage(CC.translate("&b&lArena Commands Help:"));
-        player.sendMessage(CC.translate(" &f● &b/arena create &8(&7arenaName&8) &7| Create an arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena delete &8(&7arenaName&8) &7| Delete an arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena list &7| List all arenas"));
-        player.sendMessage(CC.translate(" &f● &b/arena view &8(&7arenaName&8) &7| View arena information"));
-        player.sendMessage(CC.translate(" &f● &b/arena kitlist &8(&7arenaName&8) &7| List all kits for an arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena setcenter &8(&7arenaName&8) &7| Set center position"));
-        player.sendMessage(CC.translate(" &f● &b/arena setcuboid &8(&7arenaName&8) &7| Set min and max position"));
-        player.sendMessage(CC.translate(" &f● &b/arena setspawn &8(&7arenaName&8) &8<&7pos1/pos2&8> &7| Set spawn positions"));
-        player.sendMessage(CC.translate(" &f● &b/arena removekit &8(&7arenaName&8) &8(&7kitName&8) &7| Remove arena kit"));
-        player.sendMessage(CC.translate(" &f● &b/arena addkit &8(&7arenaName&8) &8(&7kitName&8) &7| Add a kit to an arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena teleport &8(&7arenaName&8) &7| Teleport to an arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena toggle &8(&7arenaName&8) &7| Enable or Disable an Arena"));
-        player.sendMessage(CC.translate(" &f● &b/arena tool &7| Get the Arena Selection tool"));
-        player.sendMessage(CC.translate(" &f● &b/arena saveall &7| Save all arenas"));
-        player.sendMessage("");
-        player.sendMessage(CC.translate("&b&lFFA Arena Help:"));
-        player.sendMessage(CC.translate(" &f● &b/arena setsafezone &8(&7arenaName&8) &8<&7pos1/pos2&8> &7| Set safezone positions"));
-        player.sendMessage("");
+        if (args.length > 0) {
+            try {
+                page = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                sender.sendMessage(CC.translate("&cInvalid page number."));
+            }
+        }
+
+        if (page > pages.length || page < 1) {
+            sender.sendMessage(CC.translate("&cNo more pages available."));
+            return;
+        }
+
+        sender.sendMessage("");
+        sender.sendMessage(CC.translate("&b&lArena Commands &8(&7Page &f" + page + "&7/&f" + pages.length + "&8)"));
+        for (String string : pages[page - 1]) {
+            sender.sendMessage(CC.translate(string));
+        }
+        sender.sendMessage("");
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ClickableUtil.sendPageNavigation(player, page, pages.length, "/arena", false, true);
+        }
     }
+
+    private final String[][] pages = {
+            {
+                    " &f● &b/arena list &7| List all arenas",
+                    " &f● &b/arena kitlist &8(&7arenaName&8) &7| List all kits for an arena",
+                    " &f● &b/arena saveall &7| Save all arenas",
+                    " &f● &b/arena view &8(&7arenaName&8) &7| View arena information",
+                    " &f● &b/arena delete &8(&7arenaName&8) &7| Delete an arena",
+                    " &f● &b/arena create &8(&7arenaName&8) &7| Create an arena",
+                    " &f● &b/arena tool &7| Get the Arena Selection tool"
+            },
+            {
+                    " &f● &b/arena setdisplayname &8(&7arenaName&8) &8(&7displayname&8) &7| Set display-name of an arena",
+                    " &f● &b/arena setcenter &8(&7arenaName&8) &7| Set center position",
+                    " &f● &b/arena setcuboid &8(&7arenaName&8) &7| Set min and max position",
+                    " &f● &b/arena setspawn &8(&7arenaName&8) &8<&7pos1/pos2&8> &7| Set spawn positions",
+                    " &f● &b/arena removekit &8(&7arenaName&8) &8(&7kitName&8) &7| Remove arena kit",
+                    " &f● &b/arena teleport &8(&7arenaName&8) &7| Teleport to an arena",
+                    " &f● &b/arena toggle &8(&7arenaName&8) &7| Enable or Disable an Arena",
+                    " &f● &b/arena addkit &8(&7arenaName&8) &8(&7kitName&8) &7| Add a kit to an arena"
+            },
+            {
+                    " &f● &b/arena setsafezone &8(&7arenaName&8) &8<&7pos1/pos2&8> &7| Set safezone positions for ffa"
+            }
+    };
 }
