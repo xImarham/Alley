52e50b49dbccf195044146233c433622116f1546
Emmy <lisariesey@gmail.com>
2024-11-12 15:40:11 +0100

    chore(themes): Changed my mind, I'm not adding themes. So I deleted it for now. Also wrong approach.

--- src/main/java/dev/revere/alley/Alley.java
+++ src/main/java/dev/revere/alley/Alley.java
@@ -76,7 +76,6 @@ import dev.revere.alley.profile.settings.command.toggle.ToggleTablistCommand;
 import dev.revere.alley.profile.settings.command.worldtime.*;
 import dev.revere.alley.profile.shop.command.ShopCommand;
 import dev.revere.alley.profile.shop.command.admin.SetCoinsCommand;
-import dev.revere.alley.visual.scoreboard.theme.command.ThemesCommand;
 import dev.revere.alley.queue.QueueRepository;
 import dev.revere.alley.queue.command.admin.QueueCommand;
 import dev.revere.alley.queue.command.player.LeaveQueueCommand;
@@ -304,7 +303,6 @@ public class Alley extends JavaPlugin {
             new ChallengesCommand();
             new ProfileMenuCommand();
             new MatchHistoryCommand();
-            new ThemesCommand();
             new KitEditorCommand();
         });
     }
