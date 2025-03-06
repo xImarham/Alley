f4f82bca6c098c63173e54f25076d0ec7a434325
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-03-06 17:34:39 +0100

    chore(arena-view-command): display height limit for standalone arenas.

--- src/main/java/dev/revere/alley/feature/arena/command/impl/manage/ArenaViewCommand.java
+++ src/main/java/dev/revere/alley/feature/arena/command/impl/manage/ArenaViewCommand.java
@@ -40,26 +40,26 @@ public class ArenaViewCommand extends BaseCommand {
         sender.sendMessage(CC.translate(" &f● &bDisplay Name: &f" + arena.getDisplayName()));
         sender.sendMessage(CC.translate(" &f● &bName: &f" + arena.getName()));
         sender.sendMessage(CC.translate(" &f● &bType: &f" + arena.getType()));
-        sender.sendMessage(CC.translate("   &f● &bData: &f"));
-        sender.sendMessage(CC.translate("    &f● &bCenter: &f" + (arena.getCenter() != null ? arena.getCenter().getX() + ", " + arena.getCenter().getY() + ", " + arena.getCenter().getZ() + ", &7" + arena.getCenter().getPitch() + ", " + arena.getCenter().getYaw() + " &7[" + arena.getCenter().getWorld().getName() + "]" : "&cNull")));
-        sender.sendMessage(CC.translate("    &f● &bPos1: &f" + (arena.getPos1() != null ? arena.getPos1().getX() + ", " + arena.getPos1().getY() + ", " + arena.getPos1().getZ() + ", &7" + arena.getPos1().getPitch() + ", " + arena.getPos1().getYaw() + " &7[" + arena.getPos1().getWorld().getName() + "]" : "&cNull")));
-        sender.sendMessage(CC.translate("    &f● &bPos2: &f" + (arena.getPos2() != null ? arena.getPos2().getX() + ", " + arena.getPos2().getY() + ", " + arena.getPos2().getZ() + ", &7" + arena.getPos2().getPitch() + ", " + arena.getPos2().getYaw() + " &7[" + arena.getPos2().getWorld().getName() + "]" : "&cNull")));
+        sender.sendMessage(CC.translate("    &b&lData: &f"));
+        sender.sendMessage(CC.translate("    &f• &bCenter: &f" + (arena.getCenter() != null ? arena.getCenter().getX() + ", " + arena.getCenter().getY() + ", " + arena.getCenter().getZ() + ", &7" + arena.getCenter().getPitch() + ", " + arena.getCenter().getYaw() + " &7[" + arena.getCenter().getWorld().getName() + "]" : "&cNull")));
+        sender.sendMessage(CC.translate("    &f• &bPos1: &f" + (arena.getPos1() != null ? arena.getPos1().getX() + ", " + arena.getPos1().getY() + ", " + arena.getPos1().getZ() + ", &7" + arena.getPos1().getPitch() + ", " + arena.getPos1().getYaw() + " &7[" + arena.getPos1().getWorld().getName() + "]" : "&cNull")));
+        sender.sendMessage(CC.translate("    &f• &bPos2: &f" + (arena.getPos2() != null ? arena.getPos2().getX() + ", " + arena.getPos2().getY() + ", " + arena.getPos2().getZ() + ", &7" + arena.getPos2().getPitch() + ", " + arena.getPos2().getYaw() + " &7[" + arena.getPos2().getWorld().getName() + "]" : "&cNull")));
 
         if (arena instanceof StandAloneArena) {
             StandAloneArena standAloneArena = (StandAloneArena) arena;
-            sender.sendMessage(CC.translate("    &f● &bTeam 1 Portal: &f" + (standAloneArena.getTeam1Portal() != null ? standAloneArena.getTeam1Portal().getX() + ", " + standAloneArena.getTeam1Portal().getY() + ", " + standAloneArena.getTeam1Portal().getZ() + " &7[" + standAloneArena.getTeam1Portal().getWorld().getName() + "]" : "&cNull")));
-            sender.sendMessage(CC.translate("    &f● &bTeam 2 Portal: &f" + (standAloneArena.getTeam2Portal() != null ? standAloneArena.getTeam2Portal().getX() + ", " + standAloneArena.getTeam2Portal().getY() + ", " + standAloneArena.getTeam2Portal().getZ() + " &7[" + standAloneArena.getTeam2Portal().getWorld().getName() + "]" : "&cNull")));
-            sender.sendMessage(CC.translate("    &f● &bHeight Limit: &f" + standAloneArena.getHeightLimit()));
+            sender.sendMessage(CC.translate("    &f• &bPortal 1: &f" + (standAloneArena.getTeam1Portal() != null ? standAloneArena.getTeam1Portal().getX() + ", " + standAloneArena.getTeam1Portal().getY() + ", " + standAloneArena.getTeam1Portal().getZ() + " &7[" + standAloneArena.getTeam1Portal().getWorld().getName() + "]" : "&cNull")));
+            sender.sendMessage(CC.translate("    &f• &bPortal 2: &f" + (standAloneArena.getTeam2Portal() != null ? standAloneArena.getTeam2Portal().getX() + ", " + standAloneArena.getTeam2Portal().getY() + ", " + standAloneArena.getTeam2Portal().getZ() + " &7[" + standAloneArena.getTeam2Portal().getWorld().getName() + "]" : "&cNull")));
+            sender.sendMessage(CC.translate("    &f• &bHeight Limit: &f" + standAloneArena.getHeightLimit()));
         }
 
-        sender.sendMessage(CC.translate("    &f● &bCuboid:"));
-        sender.sendMessage(CC.translate("     &f● &bMinimum: &f" + (arena.getMinimum() != null ? arena.getMinimum().getX() + ", " + arena.getMinimum().getY() + ", " + arena.getMinimum().getZ() + " &7[" + arena.getMinimum().getWorld().getName() + "]" : "&cNull")));
-        sender.sendMessage(CC.translate("     &f● &bMaximum: &f" + (arena.getMaximum() != null ? arena.getMaximum().getX() + ", " + arena.getMaximum().getY() + ", " + arena.getMaximum().getZ() + " &7[" + arena.getMaximum().getWorld().getName() + "]" : "&cNull")));
-        sender.sendMessage(CC.translate("   &f● &bKits: &f(" + arena.getKits().size() + ")"));
+        sender.sendMessage(CC.translate("    &f• &bCuboid:"));
+        sender.sendMessage(CC.translate("     &f- &bMinimum: &f" + (arena.getMinimum() != null ? arena.getMinimum().getX() + ", " + arena.getMinimum().getY() + ", " + arena.getMinimum().getZ() + " &7[" + arena.getMinimum().getWorld().getName() + "]" : "&cNull")));
+        sender.sendMessage(CC.translate("     &f- &bMaximum: &f" + (arena.getMaximum() != null ? arena.getMaximum().getX() + ", " + arena.getMaximum().getY() + ", " + arena.getMaximum().getZ() + " &7[" + arena.getMaximum().getWorld().getName() + "]" : "&cNull")));
+        sender.sendMessage(CC.translate("   &f• &bKits: &f(" + arena.getKits().size() + ")"));
         if (arena.getKits().isEmpty()) {
-            sender.sendMessage(CC.translate("    &f● &cNo Kits added yet."));
+            sender.sendMessage(CC.translate("    &f- &cNo Kits added yet."));
         } else {
-            arena.getKits().forEach(kit -> sender.sendMessage(CC.translate("    &f● &b" + kit)));
+            arena.getKits().forEach(kit -> sender.sendMessage(CC.translate("    &f- &b" + kit)));
         }
     }
 }
