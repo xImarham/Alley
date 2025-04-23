eeaed19340163f54b1d16a7f88ff200ecd4faea5
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-04-23 23:18:27 +0200

    chore(match-repository): improved "createAndStartMatch" efficiency :v

--- src/main/java/dev/revere/alley/game/match/MatchRepository.java
+++ src/main/java/dev/revere/alley/game/match/MatchRepository.java
@@ -41,24 +41,26 @@ public class MatchRepository {
      * @param participantB The second participant in the match.
      */
     public void createAndStartMatch(Kit kit, AbstractArena arena, GameParticipant<MatchGamePlayerImpl> participantA, GameParticipant<MatchGamePlayerImpl> participantB) {
-        for (Queue queue : Alley.getInstance().getQueueService().getQueues()) {
-            if (queue.getKit().equals(kit) && !queue.isRanked()) {
-                if (queue.getKit().isSettingEnabled(KitSettingLivesImpl.class)) {
-                    AbstractMatch match = new MatchLivesRegularImpl(queue, kit, arena, false, participantA, participantB);
-                    match.startMatch();
-                } else if (queue.getKit().isSettingEnabled(KitSettingBattleRushImpl.class)) {
-                    AbstractMatch match = new MatchRoundsRegularImpl(queue, kit, arena, false, participantA, participantB, 3);
-                    match.startMatch();
-                } else if (queue.getKit().isSettingEnabled(KitSettingStickFightImpl.class)) {
-                    AbstractMatch match = new MatchStickFightImpl(queue, kit, arena, false, participantA, participantB, 5);
-                    match.startMatch();
-                } else {
-                    AbstractMatch match = new MatchRegularImpl(queue, kit, arena, false, participantA, participantB);
-                    match.startMatch();
-                }
-            }
+        Queue matchingQueue = Alley.getInstance().getQueueService().getQueues().stream()
+            .filter(queue -> queue.getKit().equals(kit) && !queue.isRanked())
+            .findFirst()
+            .orElse(null);
+
+        AbstractMatch match;
+
+        if (kit.isSettingEnabled(KitSettingLivesImpl.class)) {
+            match = new MatchLivesRegularImpl(matchingQueue, kit, arena, false, participantA, participantB);
+        } else if (kit.isSettingEnabled(KitSettingBattleRushImpl.class)) {
+            match = new MatchRoundsRegularImpl(matchingQueue, kit, arena, false, participantA, participantB, 3);
+        } else if (kit.isSettingEnabled(KitSettingStickFightImpl.class)) {
+            match = new MatchStickFightImpl(matchingQueue, kit, arena, false, participantA, participantB, 5);
+        } else {
+            match = new MatchRegularImpl(matchingQueue, kit, arena, false, participantA, participantB);
         }
+
+        match.startMatch();
     }
+
 
     public void endPresentMatches() {
         if (this.matches.isEmpty()) {
