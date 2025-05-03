8c2c1548d33471c91c9ba68a825587401c4a5e17
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-05-03 10:41:27 +0200

    chore(mongo-utility): renamed document string for data classes

--- src/main/java/dev/revere/alley/database/util/MongoUtility.java
+++ src/main/java/dev/revere/alley/database/util/MongoUtility.java
@@ -54,9 +54,9 @@ public class MongoUtility {
         profileDataDocument.put("unrankedKitData", convertUnrankedKitData(profileData.getUnrankedKitData()));
         profileDataDocument.put("rankedKitData", convertRankedKitData(profileData.getRankedKitData()));
         profileDataDocument.put("ffaData", convertFFAData(profileData.getFfaData()));
-        profileDataDocument.put("profileSettingData", convertProfileSettingData(profileData.getSettingData()));
-        profileDataDocument.put("profileCosmeticData", convertProfileCosmeticData(profileData.getCosmeticData()));
-        profileDataDocument.put("profilePlayTimeData", convertProfilePlayTimeData(profileData.getPlayTimeData()));
+        profileDataDocument.put("settingData", convertProfileSettingData(profileData.getSettingData()));
+        profileDataDocument.put("settingData", convertProfileCosmeticData(profileData.getCosmeticData()));
+        profileDataDocument.put("playTimeData", convertProfilePlayTimeData(profileData.getPlayTimeData()));
 
         document.put("profileData", profileDataDocument);
         return document;
@@ -203,9 +203,9 @@ public class MongoUtility {
             existingFFAData.putAll(newFFAData);
             profileData.setFfaData(existingFFAData);
 
-            profileData.setSettingData(parseProfileSettingData((Document) profileDataDocument.get("profileSettingData")));
-            profileData.setCosmeticData(parseProfileCosmeticData((Document) profileDataDocument.get("profileCosmeticData")));
-            profileData.setPlayTimeData(parseProfilePlayTimeData((Document) profileDataDocument.get("profilePlayTimeData")));
+            profileData.setSettingData(parseProfileSettingData((Document) profileDataDocument.get("settingData")));
+            profileData.setCosmeticData(parseProfileCosmeticData((Document) profileDataDocument.get("settingData")));
+            profileData.setPlayTimeData(parseProfilePlayTimeData((Document) profileDataDocument.get("playTimeData")));
 
             profile.setProfileData(profileData);
         }
