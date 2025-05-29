1b138ae226137e3b4d077bf681ca91cbfd938195
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-05-29 12:16:30 +0200

    feat(base-command): added "getOfflineProfile" to simplify profile lookup by name. Other commands will be updated for consistency.

--- src/main/java/dev/revere/alley/api/command/BaseCommand.java
+++ src/main/java/dev/revere/alley/api/command/BaseCommand.java
@@ -1,6 +1,13 @@
 package dev.revere.alley.api.command;
 
 import dev.revere.alley.Alley;
+import dev.revere.alley.profile.Profile;
+import dev.revere.alley.util.PlayerUtil;
+import dev.revere.alley.util.chat.CC;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+
+import java.util.UUID;
 
 public abstract class BaseCommand {
     public final Alley plugin;
@@ -19,4 +26,33 @@ public abstract class BaseCommand {
      * @param command The command.
      */
     public abstract void onCommand(CommandArgs command);
+
+    /**
+     * Either fetches the profile of an online player or retrieves the offline profile.
+     *
+     * @param target The name of the player.
+     * @param sender The command sender.
+     * @return The profile of the player, or null if not found.
+     */
+    public Profile getOfflineProfile(String target, CommandSender sender) {
+        OfflinePlayer offlinePlayer = PlayerUtil.getOfflinePlayerByName(target);
+        if (offlinePlayer == null) {
+            sender.sendMessage(CC.translate("&cThat player does not exist."));
+            return null;
+        }
+
+        UUID uuid = offlinePlayer.getUniqueId();
+        if (uuid == null) {
+            sender.sendMessage(CC.translate("&cThat player is invalid."));
+            return null;
+        }
+
+        Profile profile = this.plugin.getProfileService().getProfile(uuid);
+        if (profile == null) {
+            sender.sendMessage(CC.translate("&cThat player does not have a profile."));
+            return null;
+        }
+
+        return profile;
+    }
 }
