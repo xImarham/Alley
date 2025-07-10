76c1a9aa1e28fab566fca7d17287e94360e28b2a
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-07-10 19:23:30 +0200

    chore: fancy ArenaListCommand with clickables and hovers

--- src/main/java/dev/revere/alley/base/arena/command/impl/manage/ArenaListCommand.java
+++ src/main/java/dev/revere/alley/base/arena/command/impl/manage/ArenaListCommand.java
@@ -4,35 +4,74 @@ import dev.revere.alley.Alley;
 import dev.revere.alley.api.command.BaseCommand;
 import dev.revere.alley.api.command.CommandArgs;
 import dev.revere.alley.api.command.annotation.CommandData;
+import dev.revere.alley.base.arena.AbstractArena;
 import dev.revere.alley.base.arena.IArenaService;
-import dev.revere.alley.base.arena.enums.EnumArenaType;
+import dev.revere.alley.tool.visual.TextFormatter;
 import dev.revere.alley.util.chat.CC;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.Comparator;
+
 /**
- * @author Remi
+ * @author Emmy
  * @project Alley
- * @date 5/20/2024
+ * @date 10/07/2025
  */
 public class ArenaListCommand extends BaseCommand {
+
     @CommandData(name = "arena.list", aliases = {"arenas"}, isAdminOnly = true)
     @Override
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
+
         IArenaService arenaService = Alley.getInstance().getService(IArenaService.class);
 
         player.sendMessage("");
         player.sendMessage(CC.translate("     &6&lArena List &f(" + arenaService.getArenas().size() + "&f)"));
+
         if (arenaService.getArenas().isEmpty()) {
-            player.sendMessage(CC.translate("      &f● &cNo Arenas available."));
+            player.sendMessage(CC.translate("      &f● &cNo arenas available."));
+            return;
         }
 
-        arenaService.getArenas().stream().filter(arena -> arena.getType() != EnumArenaType.FFA).forEach(arena ->
-                player.sendMessage(CC.translate("      &f● &6" + arena.getName() + " &7(" + arena.getType().name() + ")" + (arena.isEnabled() ? " &aEnabled" : " &cDisabled"))))
-        ;
-        arenaService.getArenas().stream().filter(arena -> arena.getType() == EnumArenaType.FFA).forEach(arena ->
-                player.sendMessage(CC.translate("      &f● &6" + arena.getName() + " &7(" + arena.getType().name() + ")")))
-        ;
+        arenaService.getArenas().stream()
+                .sorted(Comparator.comparing(AbstractArena::getName))
+                .forEach(arena -> {
+                    ComponentBuilder hover = new ComponentBuilder("")
+                            .append(CC.translate("&6&lArena Info:\n"))
+                            .append(CC.translate(" &f● Display Name: &6" + arena.getDisplayName() + "\n"))
+                            .append(CC.translate(" &f● Center: &6" + TextFormatter.formatLocation(arena.getCenter()) + "\n"))
+                            .append(CC.translate(" &f● Pos1: &6" + TextFormatter.formatLocation(arena.getPos1()) + "\n"))
+                            .append(CC.translate(" &f● Pos2: &6" + TextFormatter.formatLocation(arena.getPos2()) + "\n"));
+
+                    ComponentBuilder message = new ComponentBuilder("      ");
+                    message.event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hover.create()));
+
+                    message.append("● ").color(ChatColor.WHITE.asBungee());
+                    message.append(arena.getName()).color(ChatColor.GOLD.asBungee());
+                    message.append(" - ").color(ChatColor.WHITE.asBungee());
+
+                    message.append("[TP] ")
+                            .color(ChatColor.GREEN.asBungee())
+                            .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/arena teleport " + arena.getName()))
+                            .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to teleport to " + arena.getName()).color(ChatColor.GREEN.asBungee()).create()));
+
+                    message.append("[X] ")
+                            .color(ChatColor.RED.asBungee())
+                            .event(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/arena delete " + arena.getName()))
+                            .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to delete " + arena.getName()).color(ChatColor.RED.asBungee()).create()));
+
+                    message.append("[i]")
+                            .color(ChatColor.GRAY.asBungee())
+                            .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/arena view " + arena.getName()))
+                            .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click to view info for " + arena.getName()).color(ChatColor.GRAY.asBungee()).create()));
+
+                    player.spigot().sendMessage(message.create());
+                });
 
         player.sendMessage("");
     }
