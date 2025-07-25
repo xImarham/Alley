706c2e057e288a80630ecce29e2990fc92efbc7a
hmRemi <remig06@hotmail.com>
2025-07-25 23:35:22 +0200

    chore: added party and state check for party event menu command

--- src/main/java/dev/revere/alley/game/party/command/impl/leader/PartyEventMenuCommand.java
+++ src/main/java/dev/revere/alley/game/party/command/impl/leader/PartyEventMenuCommand.java
@@ -3,7 +3,12 @@ package dev.revere.alley.game.party.command.impl.leader;
 import dev.revere.alley.api.command.BaseCommand;
 import dev.revere.alley.api.command.CommandArgs;
 import dev.revere.alley.api.command.annotation.CommandData;
+import dev.revere.alley.config.locale.impl.PartyLocale;
 import dev.revere.alley.game.party.menu.event.PartyEventMenu;
+import dev.revere.alley.profile.IProfileService;
+import dev.revere.alley.profile.Profile;
+import dev.revere.alley.profile.enums.EnumProfileState;
+import dev.revere.alley.util.chat.CC;
 import org.bukkit.entity.Player;
 
 /**
@@ -16,6 +21,18 @@ public class PartyEventMenuCommand extends BaseCommand {
     @Override
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
+        Profile profile = plugin.getService(IProfileService.class).getProfile(player.getUniqueId());
+
+        if (profile.getParty() == null) {
+            player.sendMessage(CC.translate(PartyLocale.NOT_IN_PARTY.getMessage()));
+            return;
+        }
+
+        if (profile.getState() != EnumProfileState.LOBBY) {
+            player.sendMessage(CC.translate("&cYou are not in the lobby!"));
+            return;
+        }
+
         new PartyEventMenu().openMenu(player);
     }
 }
